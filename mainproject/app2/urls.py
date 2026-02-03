a21d64a9bcfd13cd1c5f675e5e10d7a87bc65215
Srujan <Srujan2004ab@gmail.com>
2026-02-03 14:49:21 +0530

    Differnts pages created

--- mainproject/app2/urls.py
+++ mainproject/app2/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
-from app2.views import html
+from app2.views import html,About,Contact,MyProfile,doahboard,Shopping
 
 
 
 urlpatterns = [
-      path('app2',html,name='html'),
+      path('app2/',html,name='html'),
+      path('app2/About.html',About,name='About.html'),
+      path('app2/Shopping.html',Shopping,name='Shopping.html'),
+      path('app2/Contact.html',Contact,name='Contact.html'),
+      path('app2/MyProfile.html',MyProfile,name='MyProfile.html'),
+      path('app2/doahboard.html',doahboard,name='doahbord.html'),
+      
+      
+      
+      
       
 ]
--- mainproject/app2/views.py
+++ mainproject/app2/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 
 def html(request):
     return render(request,'html.html')
+
+def About(request):
+    return render(request,'About.html')
+
+def Contact(request):
+    return render(request,'Contact.html')
+
+def MyProfile(request):
+    return render(request,'Myprofile.html')
+
+def doahboard(request):
+    return render(request,'doahboard.html')
+
+def Shopping(request):
+    return render(request,'Shopping.html')
+
