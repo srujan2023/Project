e82ea578375a652d65495ff954e15a33e6ed9f44
Srujan <Srujan2004ab@gmail.com>
2026-02-09 15:49:48 +0530

    register css updated

--- mainproject/app1/models.py
+++ mainproject/app1/models.py
@@ -3,14 +3,15 @@ from django.db import models
 class Teachers(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField(null=True,blank=True)
+    Password = models.CharField(null=True,blank=True)
     
-    enrollment_date = models.DateField(null=True,blank=True)
+    # enrollment_date = models.DateField(null=True,blank=True)
     Address = models.CharField(null=True,blank=True)
     
     DOB = models.DateField(null=True,blank=True)
-    Teachers_id = models.IntegerField(null=True,blank=True)
+    # Teachers_id = models.IntegerField(null=True,blank=True)
     
-    Collage_Name = models.CharField(null=True,blank=True)
+    # Collage_Name = models.CharField(null=True,blank=True)
     
     Gender = models.CharField(null=True,blank=True)
     def __str__(self):
--- mainproject/app1/views.py
+++ mainproject/app1/views.py
@@ -12,21 +12,23 @@ def regsiter(request):
     if request.method=='POST':
         name=request.POST.get('name')
         age=request.POST.get('age')
-        enrollment_date=request.POST.get('enrollment_date')
+        Password=request.POST.get('Password')
+        # enrollment_date=request.POST.get('enrollment_date')
         Address=request.POST.get('Address')
         DOB=request.POST.get('DOB')
-        Teachers_id=request.POST.get('Teachers_id')
-        Collage_name=request.POST.get('Collage_name')
+        # Teachers_id=request.POST.get('Teachers_id')
+        # Collage_name=request.POST.get('Collage_name')
         Gender=request.POST.get('Gender')
         
         stu_obj=Teachers()
         stu_obj.name=name
         stu_obj.age=age
-        stu_obj.enrollment_date=enrollment_date
+        stu_obj.Password=Password
+        # stu_obj.enrollment_date=enrollment_date
         stu_obj.Address=Address
         stu_obj.DOB=DOB
-        stu_obj.Teachers_id=Teachers_id
-        stu_obj.Collage_Name=Collage_name
+        # stu_obj.Teachers_id=Teachers_id
+        # stu_obj.Collage_Name=Collage_name
         stu_obj.Gender=Gender
         stu_obj.save()
         #print("Teacher_name":,name)
@@ -38,6 +40,7 @@ def regsiter(request):
         # print("callagename",callagename)
         # print("id:",id)
         # print("gender:",gender)   
+        return redirect("/") 
     return render(request,'teachers_register.html')
 
 def update(request,id):
@@ -45,10 +48,12 @@ def update(request,id):
     if request.method=='POST':
         stu_obj.name=request.POST.get('name')
         stu_obj.age=request.POST.get('age')
-        stu_obj.enrollment_date=request.POST.get('enrollment_date')
+        stu_obj.Password=request.POST.get('Password')
+        
+        # stu_obj.enrollment_date=request.POST.get('enrollment_date')
         stu_obj.Address=request.POST.get('Address')
         stu_obj.DOB=request.POST.get('DOB')
-        stu_obj.Teachers_id=request.POST.get('Teachers_id')
+        # stu_obj.Teachers_id=request.POST.get('Teachers_id')
         stu_obj.Collage_Name=request.POST.get('Collage_Name')
         stu_obj.Gender=request.POST.get('Gender')
         stu_obj.save()
