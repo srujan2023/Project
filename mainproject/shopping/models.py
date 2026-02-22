501739609238d604ee76c151f0584153ad98df62
Srujan <Srujan2004ab@gmail.com>
2026-02-22 18:34:12 +0530

    Shoppingcart Model Updated

--- mainproject/shopping/models.py
+++ mainproject/shopping/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Shopping(models.Model):
+    Productname = models.CharField(max_length=100)
+    Price = models.IntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.Productname
+
