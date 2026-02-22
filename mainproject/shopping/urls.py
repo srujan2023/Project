d4f9ddcc68cac6a198ce70a0e26fe9c7aff07466
Srujan <Srujan2004ab@gmail.com>
2026-02-22 18:50:14 +0530

    Shopping cart Model and HTML page updated

--- mainproject/shopping/urls.py
+++ mainproject/shopping/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from shopping.views import cart
+from shopping.views import cart,create_cart
 
 urlpatterns = [
     
      path('cart', cart, name='cart'),
+     path('createcart', create_cart, name='create_cart'),
+     
 ]
--- mainproject/shopping/views.py
+++ mainproject/shopping/views.py
@@ -1,6 +1,26 @@
+from urllib import request
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Shopping
+
 
 def cart(request):
     return render(request,"Shopping.html")
 
+def create_cart(request):
+    if request.method=='POST':
+        Productname=request.POST.get('Productname')
+        Price=request.POST.get('Price')
+        description=request.POST.get('description')
+        
+        stu_obj=Shopping()
+        stu_obj.Productname=Productname
+        stu_obj.Price=Price
+        stu_obj.description=description
+        stu_obj.save()
+        
+        print(Productname,Price,description)
+        return HttpResponse("Cart created successfully")        
+
+    return render(request,"create_cart.html")
