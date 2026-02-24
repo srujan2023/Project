c40135cd69f7b294392391e11ffbbaaefc9856c9
Srujan <Srujan2004ab@gmail.com>
2026-02-24 22:51:53 +0530

    payment option

--- mainproject/shopping/urls.py
+++ mainproject/shopping/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from shopping.views import cart,create_cart
+from shopping.views import cart,create_cart,views
 
 urlpatterns = [
     
      path('cart', cart, name='cart'),
      path('createcart', create_cart, name='create_cart'),
+      path('buy/<int:product_id>/', views.buy_product, name='buy_product'),
+    path('payment-success/', views.payment_success, name='payment_success'),
      
 ]
--- mainproject/shopping/views.py
+++ mainproject/shopping/views.py
@@ -26,3 +26,34 @@ def create_cart(request):
         return redirect('cart')
 
     return render(request, "create_cart.html")
+
+
+import razorpay
+from django.conf import settings
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def buy_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
+
+    payment = client.order.create({
+        "amount": int(product.Price * 100),  # Razorpay takes amount in paise
+        "currency": "INR",
+        "payment_capture": "1"
+    })
+
+    context = {
+        "product": product,
+        "payment": payment,
+        "razorpay_key": settings.RAZORPAY_KEY_ID
+    }
+
+    return render(request, "payment.html", context)
+
+
+from django.http import HttpResponse
+
+def payment_success(request):
+    return HttpResponse("Payment Successful 🎉")
